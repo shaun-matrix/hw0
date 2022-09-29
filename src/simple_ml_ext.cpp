c570b48e6ce0035dd0554b02c68b7f4bcca868f0
yaoxuan <yaoxuan@airdoc.com>
2022-09-29 10:19:44 +0800

    20220928 HW0 submission

--- src/simple_ml_ext.cpp
+++ src/simple_ml_ext.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 namespace py = pybind11;
-
+using namespace std;
 
 void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
 								  float *theta, size_t m, size_t n, size_t k,
@@ -33,7 +33,68 @@ void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
      */
 
     /// BEGIN YOUR CODE
+    int iters_per_epoch = m / batch;
+    cout << "iters_per_epoch: " << iters_per_epoch << endl;
+    float identity_matrix[batch][k];
+    float z_matrix[batch][k];
+    float gradient[n][k];
 
+    for(int iter = 0; iter < iters_per_epoch; iter++){
+        // locate X, y pointer in each batch
+        const float *X_batch = X + iter*batch*n;
+        const unsigned char *y_batch = y + iter*batch;
+
+        for(size_t i = 0; i < batch; i++){
+          // construct an identity matrix
+          unsigned char label = y_batch[i];
+          for(size_t j = 0; j < k; j++){
+              if(j == label){
+                identity_matrix[i][j] = 1;
+              }
+              else{
+                identity_matrix[i][j] = 0;
+              }
+          }
+        }
+
+        for(size_t i = 0; i < batch; i++){
+          // to normalize row-wise
+          float sum_row = 0;
+          // calculate exp(X * theta) and record sum row-wise
+          for(size_t j = 0; j < k; j++){
+            z_matrix[i][j] = 0;
+            for(size_t l = 0; l < n; l++){
+              z_matrix[i][j] += X_batch[i*n + l] * theta[l*k + j];
+            }
+            z_matrix[i][j] = exp(z_matrix[i][j]);
+            sum_row += z_matrix[i][j];
+
+          }
+          // calculate Z = normalize(exp(X*theta)) and Z-I_y
+          for(size_t j = 0; j < k; j++){
+            z_matrix[i][j] = z_matrix[i][j] / sum_row - identity_matrix[i][j];
+          }
+        }
+
+        // calculate gradient per batch
+        for(size_t l=0; l<n; l++){
+          for(size_t j=0; j<k; j++){
+            gradient[l][j] = 0;
+            for(size_t i=0; i<batch; i++){
+              gradient[l][j] += X_batch[i*n+l] * z_matrix[i][j];
+            }
+            gradient[l][j] /= batch;
+          }
+        }
+
+        // update theta; theta -= lr*gradient
+        for(size_t l=0; l<n; l++){
+          for(size_t j=0; j<k; j++){
+            theta[l*k+j] -= lr * gradient[l][j];
+          }
+        }
+
+    }
     /// END YOUR CODE
 }
 
